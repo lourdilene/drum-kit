7b87ddd598dd7a2d60c3848c0653f9ab12605c75
Lourdilene Souza <lourdilene.souza@gmail.com>
2022-11-04 16:23:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var buttons = document.querySelectorAll(".drum");
+
+for (let index = 0; index < buttons.length; index++) {
+    document.querySelectorAll(".drum")[index].addEventListener("click", function() {
+
+        const buttonInnerHTML = this.innerHTML;
+        
+        makeSound(buttonInnerHTML);        
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);   
+});
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();                
+        break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "k":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+        break;
+
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+    
+        default:
+            console.log(buttonInnerHTML);
+            break;
+    }
+}
